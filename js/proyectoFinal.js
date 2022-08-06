6a4fdfbdad01671f085169d1a4606ed7090a3224
Eric Dehner <eric.dehner.ga@gmail.com>
2022-08-06 17:49:40 -0300

    aplicando dom

--- js/proyectoFinal.js
+++ js/proyectoFinal.js
@@ -1,58 +1,71 @@
-//saludo
-alert(`¡Bienvenido!`)
-//funcion de inicio 
-bienvenida();
-function bienvenida() {
-    class Producto {
-        constructor(nombre, precio, personalizado) {
-            this.nombre = nombre;
-            this.precio = parseFloat(precio);
-            this.personalizado = personalizado;
-        }
+
+//array
+class Producto {
+    constructor(nombre, precio, personalizado) {
+        this.nombre = nombre;
+        this.precio = parseFloat(precio);
+        this.talle = talle;
     }
-    const productos = [];
-    let precio = 0;
-    let nameUser = prompt(`Ingrese su nombre, porfavor.`);
+}
+const productos = [
+    {
+        tipo: `Almohadon basico`,
+        precio: 1500,
+        talle: `S, M, L`,
+        img: "./imagenes/productos/almohadones/07.png"
+    },
+    {
+        tipo: `Almohadon personalizado`,
+        precio: 1800,
+        talle: `S, M, L`,
+        img: "./imagenes/productos/almohadones/01.png"
+    },
+    {
+        tipo: `Barbijo basico`,
+        precio: 400,
+        talle: `S, M, L`,
+        img: "./imagenes/productos/barbijos/4.png"
+    },
+    {
+        tipo: `Barbijo personalizado`,
+        precio: 550,
+        talle: `S, M, L`,
+        img: "./imagenes/productos/barbijos/9.png"
+    }
+];
+let precio = 0;
+
+//funcion de inicio 
+ingreso();
+function ingreso() {
+    let nameUser = prompt(`¡Bienvenido! Ingrese su nombre, porfavor.`);
     //si ingresa un nombre que no sea "", muestra productos.
     if (nameUser !== ``) {
-        alert(`Hola ${nameUser}, ¿en qué podemos ayudarte? A continuacion te detallaremos nuestros productos`)
-        adqProd = (prompt(`${nameUser}, ingrese que producto desea adquirir.\n - Almohadones basicos\n - Almohadones personalizados\n - Barbijos basicos\n - Barbijos personalizados\nPresione enter para continuar.`)).toLowerCase();
-        calcular();
-        function calcular() {
-            while (adqProd != "") {
-                switch (adqProd) {
-                    case `almohadones basicos`:
-                    case `almohadon basico`:
-                        productos.push(new Producto(`almohadon basico`, 1500, false));
-                        precio = precio + 1500;
-                        break;
-                    case `almohadones personalizados`:
-                    case `almohadon personalizado`:
-                        productos.push(new Producto(`almohadon personalizado`, 1800, true));
-                        precio = precio + 1800;
-                        break;
-                    case `barbijos basicos`:
-                    case `barbijo basico`:
-                        productos.push(new Producto(`barbijo basico`, 400, false));
-                        precio = precio + 400;
-                        break;
-                    case `barbijos personalizados`:
-                    case `barbijo personalizado`:
-                        productos.push(new Producto(`barbijo personalizado`, 550, true));
-                        precio = precio + 550;
-                        break;
-                }
-                console.table(productos)
-                adqProd = prompt(`${nameUser}, ingrese que producto desea adquirir.\n - Almohadones basicos\n - Almohadones personalizados\n - Barbijos basicos\n - Barbijos personalizados\nPresione enter para continuar.`)
-            }
-            const personalizado = productos.filter((productos) => productos.personalizado == false)
-            console.log(personalizado)
-            console.log(`Total a pagar: $ ${precio}`);
+        //cartel emergente
+        let cartel = document.querySelector(`.productos_intro`);
+        cartel.innerHTML = `${nameUser}, ¡Realizando una compra de 2 productos o más, el envío será gratis! `
+        //cards
+        let cartas = document.getElementById(`cartas`)
+        for (const producto of productos) {
+            let carta = document.createElement(`div`);
+            carta.className = `card`;
+            carta.innerHTML = `<img src="${producto.img}" class="productos_img card-img-top" alt="productos">
+    <div class="card-body">
+        <h5 class="d-flex justify-content-center align-items-center card-title">${producto.tipo}</h5>
+        <h6 class="d-flex justify-content-center align-items-center card-title talles">Talles disponibles<br>${producto.talle}</h6>
+        <div class="d-flex justify-content-around align-items-center">
+            <p class="card-text">$${producto.precio}</p>
+            <button type="button" class="btn btn-primario"><i class="fa-solid fa-cart-shopping"></i></button>
+        </div>
+    </div>
+    `;
+            cartas.append(carta);
         }
+
     }
     //si el nombre fue "", solicita ingresar nuevamente nombre.
     else {
         alert(`El nombre ingresado no es valido, por favor intentelo nuevamente.`)
-        bienvenida();
+        ingreso();
     }
-}
+    }
